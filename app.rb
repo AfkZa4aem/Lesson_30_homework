9ed5d4bb63909fbd68e003b89a257a6bae5e8075
AfkZa4aem <nastypinagrably@gmail.com>
2017-01-17 12:36:54 +0300

    save posts to db / show posts on index

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/activerecord'
 set :database,  "sqlite3:blog.db"
 
 class Post < ActiveRecord::Base
-
+	validates :content, presence: true
 end
 
 class Comment < ActiveRecord::Base
@@ -15,15 +15,21 @@ end
 
 get '/' do
 	# get posts from db
-	
+	@posts = Post.order "created_at DESC"
 	erb :index			
 end
 
 get '/new' do
-
+	@content = Post.new
 	erb :new
 end
 
 post '/new' do
-
+	@content = Post.new params[:post]
+		if @content.save
+			erb :index
+		else
+			@error = @content.errors.full_messages.first
+			erb :new
+		end
 end
